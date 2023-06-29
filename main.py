5c683e6aa5eecde40fc36cb3367c0968def115fa
Nattq <nataliajelito3@gmail.com>
2023-06-29 20:03:08 +0200

    run from main added

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-# to do
-# emploees ADDRESS TABLE!
-# emploees: address id
+from urllib.parse import quote
 
+import pandas as pd
+from sqlalchemy import create_engine, text
 
-
-
-from generate import *
-
-import pandas as pd 
-from sqlalchemy import create_engine
-from urllib.parse import quote  
+from src.generate import *
 
 username = "team02"
 password = "te@m0z"
 database = "team02"
 
-
 if __name__ == "__main__":
+
     proportions = np.array([0.4, 0.25, 0.35]) # ALL_ENG, W_PL, M_PL
     names_lists = (eng_first_names, eng_last_names, pl_first_names_w, pl_first_names_m,pl_last_names_w, pl_last_names_m)
 
     games_tbl = generate_games_tbl(new_games_df)
     tournament_games = random_games(games_tbl)
-
     phone_numbers = generate_ALL_phone_numbers(CUSTOMERS_AMOUNT+EMPLOYEES_AMOUNT)
     customers_tbl = generate_customers_tbl(names_lists, phone_numbers, proportions)
     employees_tbl = generate_employees_tbl(phone_numbers)
@@ -38,7 +31,6 @@ if __name__ == "__main__":
     temp_rent_pay = temp_rent_payments(rentals_tbl)
     temp_sales_pay = temp_sales_payments(sales_tbl, games_tbl)
     temp_fee_pay = temp_entry_fee_payments()
-    print(temp_fee_pay)
     payments_tbl = concat_payments( temp_payoff_pay, temp_sales_pay, temp_rent_pay, temp_fee_pay)
 
     temp_rent = temp_rent_invent(rentals_tbl)
@@ -49,18 +41,21 @@ if __name__ == "__main__":
     sales_tbl = drop_from_sales(sales_tbl)
     rentals_tbl = drop_from_rentals(rentals_tbl)
 
-    # engine = create_engine("mysql+pymysql://team02:%s@giniewicz.it:3306/team02" %quote(password))
-    # connection = engine.connect()
-    # games_tbl.to_sql("Games", con =  connection, if_exists='append', index=False)
-    # customers_tbl.to_sql("Customers", con =  connection, if_exists='append', index=False)
-    # employees_tbl.to_sql("Employees", con =  connection, if_exists='append', index=False)
-    # address_tbl.to_sql("Address", con =  connection, if_exists='append', index=False)
-    # termines_tbl.to_sql("Tournament_schedule", con =  connection, if_exists='append', index=False)
-    # tournaments_tbl.to_sql("Tournaments", con =  connection, if_exists='append', index=False)
-    # results_tbl.to_sql("Tournaments_results", con =  connection, if_exists='append', index=False)
-    # rentals_tbl.to_sql("Rentals", con =  connection, if_exists='append', index=False)
-    # sales_tbl.to_sql("Sales", con =  connection, if_exists='append', index=False)
-    # inventory_tbl.to_sql("Inventory", con =  connection, if_exists='append', index=False)
-    # payments_tbl.to_sql("Finances", con =  connection, if_exists='append', index=False)
-    # payoffs_tbl.to_sql("Payoffs", con =  connection, if_exists='append', index=False)
-    # connection.close()
+    engine = create_engine("mysql+pymysql://team02:%s@giniewicz.it:3306/team02" %quote(password))
+    with engine.connect() as connection:
+        with open("src/create.sql", "r") as file:
+            for statement in file.read().split(';'):
+                if len(statement.strip()) > 0:
+                    connection.execute(statement + ';')
+        games_tbl.to_sql("Games", con =  connection, if_exists='append', index=False)
+        customers_tbl.to_sql("Customers", con =  connection, if_exists='append', index=False)
+        employees_tbl.to_sql("Employees", con =  connection, if_exists='append', index=False)
+        address_tbl.to_sql("Addresses", con =  connection, if_exists='append', index=False)
+        termines_tbl.to_sql("Tournament_schedule", con =  connection, if_exists='append', index=False)
+        tournaments_tbl.to_sql("Tournaments", con =  connection, if_exists='append', index=False)
+        results_tbl.to_sql("Tournaments_results", con =  connection, if_exists='append', index=False)
+        rentals_tbl.to_sql("Rentals", con =  connection, if_exists='append', index=False)
+        sales_tbl.to_sql("Sales", con =  connection, if_exists='append', index=False)
+        inventory_tbl.to_sql("Inventory", con =  connection, if_exists='append', index=False)
+        payments_tbl.to_sql("Finances", con =  connection, if_exists='append', index=False)
+        payoffs_tbl.to_sql("Payoffs", con =  connection, if_exists='append', index=False)
---
+++ src/create.sql
@@ -0,0 +1,121 @@
+CREATE OR REPLACE TABLE Addresses
+(
+  address_id  SMALLINT    NOT NULL,
+  country     VARCHAR(50) NOT NULL,
+  city        VARCHAR(50) NOT NULL,
+  street      VARCHAR(45) NOT NULL,
+  number      VARCHAR(10)    NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  PRIMARY KEY (address_id)
+);
+
+CREATE OR REPLACE TABLE Customers
+(
+  customer_id SMALLINT    NOT NULL,
+  first_name  VARCHAR(45) NOT NULL,
+  last_name   VARCHAR(45) NOT NULL,
+  birth_date  DATE        NOT NULL,
+  email       VARCHAR(50) NOT NULL,
+  phone       VARCHAR(12) NOT NULL,
+  PRIMARY KEY (customer_id)
+);
+
+CREATE OR REPLACE  TABLE Employees
+(
+  employee_id     SMALLINT    NOT NULL,
+  first_name      VARCHAR(45) NOT NULL,
+  last_name       VARCHAR(50) NOT NULL,
+  start_work_date DATE        NOT NULL,
+  end_work_date   DATE        NULL    ,
+  email           VARCHAR(45) NOT NULL,
+  phone           INT         NOT NULL,
+  address_id      SMALLINT    NOT NULL,
+  birth_date      DATE        NOT NULL,
+  PRIMARY KEY (employee_id)
+);
+
+CREATE OR REPLACE  TABLE Finances
+(
+  payment_id INT          NOT NULL,
+  in_out     TINYINT(1)   NOT NULL,
+  value      DECIMAL      NOT NULL,
+  date       DATE         NOT NULL,
+  title      VARCHAR(100) NOT NULL,
+  PRIMARY KEY (payment_id)
+);
+
+CREATE OR REPLACE TABLE Games
+(
+  game_id      SMALLINT      NOT NULL,
+  release_year DATE          NOT NULL,
+  min_players  TINYINT       NOT NULL,
+  max_players  SMALLINT      NOT NULL,
+  play_time    VARCHAR(50)   NOT NULL,
+  min_age      TINYINT       NOT NULL,
+  price        DECIMAL       NOT NULL,
+  title        VARCHAR(100)  NOT NULL,
+  type         VARCHAR(18)   NOT NULL,
+  PRIMARY KEY (game_id)
+);
+
+CREATE OR REPLACE  TABLE Inventory
+(
+  inventory_id SMALLINT NOT NULL,
+  game_id      SMALLINT NOT NULL,
+  type         Char(1)  NULL    ,
+  PRIMARY KEY (inventory_id)
+);
+
+CREATE OR REPLACE  TABLE Payoffs
+(
+  payment_id  INT      NOT NULL,
+  employee_id SMALLINT NOT NULL,
+  PRIMARY KEY (payment_id)
+);
+
+CREATE OR REPLACE  TABLE Rentals
+(
+  rental_id    SMALLINT NOT NULL,
+  payment_id   INT      NOT NULL,
+  employee_id  SMALLINT NOT NULL,
+  customer_id  SMALLINT NOT NULL,
+  inventory_id SMALLINT NOT NULL,
+  rental_date  DATE     NOT NULL,
+  return_date  DATE     NULL    ,
+  PRIMARY KEY (rental_id)
+);
+
+CREATE OR REPLACE  TABLE Sales
+(
+  payment_id   INT      NOT NULL,
+  employee_id  SMALLINT NOT NULL,
+  customer_id  SMALLINT NOT NULL,
+  inventory_id SMALLINT NOT NULL,
+  PRIMARY KEY (payment_id)
+);
+
+CREATE OR REPLACE  TABLE Tournament_schedule
+(
+  tournament_id SMALLINT NOT NULL,
+  date          DATE     NOT NULL,
+  PRIMARY KEY (tournament_id)
+);
+
+CREATE OR REPLACE TABLE Tournaments
+(
+  tournament_id SMALLINT NOT NULL,
+  game_id       SMALLINT NOT NULL,
+  max_players   TINYINT  NOT NULL,
+  entry_fee     DECIMAL  NOT NULL,
+  prize         DECIMAL  NOT NULL,
+  PRIMARY KEY (tournament_id)
+);
+
+CREATE OR REPLACE TABLE Tournaments_results
+(
+  tournament_id SMALLINT NOT NULL,
+  place         SMALLINT NOT NULL,
+  customer_id   SMALLINT NOT NULL,
+  PRIMARY KEY (tournament_id, place)
+);
+
